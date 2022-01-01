a75571928609374b6207d8636a94284511046a74
ValadaRP <79703007+ValadaRP@users.noreply.github.com>
2022-01-01 22:01:59 +0100

    Change color in menu

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@ function openMenu(evt, menuName) {
   }
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < x.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" w3-dark-grey", "");
+    tablinks[i].className = tablinks[i].className.replace(" w3-brown", "");
   }
   document.getElementById(menuName).style.display = "block";
-  evt.currentTarget.firstElementChild.className += " w3-dark-grey";
+  evt.currentTarget.firstElementChild.className += " w3-brown";
 }
 document.getElementById("myLink").click();
